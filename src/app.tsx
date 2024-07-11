66a2357b2b4ad58db5162ed512f5cb3fc6b966e9
Antony <115037549+antonysantos71@users.noreply.github.com>
2024-07-11 01:20:45 +0000

    Initial commit

---
+++ src/app.tsx
@@ -0,0 +1,15 @@
+export function App() {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <div className="max-w-3xl px-6">
+        <p className="text-zinc-300 text-lg">Convide seus amigos e planeje sua proxima viagem!</p>
+        <div>
+        </div>
+        <p className="text-sm text-zinc-500">
+          Ao planejar sua viagem pelo Plann.er você automaticamente concorda <br /> com nossos
+          <a className="text-zinc-300 underline" href="#">termos de uso </a> e <a className="text-zinc-300 underline" href="#">politicas de provacidade</a>
+        </p>
+      </div>
+    </div>
+  )
+}
